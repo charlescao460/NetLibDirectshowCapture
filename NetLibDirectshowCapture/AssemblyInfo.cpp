f0d858e9747c36aebe67fcb907434f1d0743df65
charlescao460 <charlescao@csr460.com>
2021-01-11 00:34:03 -0500

    Init CLR solution & project

---
+++ NetLibDirectshowCapture/AssemblyInfo.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"NetLibDirectshowCapture")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"GITHUB.COM/CHARLESCAO460")];
+[assembly:AssemblyProductAttribute(L"NetLibDirectshowCapture")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright Shirui Cao (C) 2021")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
