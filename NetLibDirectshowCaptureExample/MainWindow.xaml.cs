3eac3edc56e46d20b0a510485913afef0af0074b
charlescao460 <charlescao@csr460.com>
2021-01-15 06:56:27 -0500

    Add AssemblyLinkResource to managed dll

--- NetLibDirectshowCaptureExample/MainWindow.xaml.cs
+++ NetLibDirectshowCaptureExample/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NetLibDirectshowCapture;
 
 namespace NetLibDirectshowCaptureExample
 {
@@ -23,6 +24,14 @@ namespace NetLibDirectshowCaptureExample
         public MainWindow()
         {
             InitializeComponent();
+            ListVideoDevices();
         }
+
+        private void ListVideoDevices()
+        {
+            var devices = Device.EnumVideoDevices();
+            return;
+        }
+
     }
 }
